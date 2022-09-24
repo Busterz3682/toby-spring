8bfa9a7e0601e3c3b3df07a54ce359126806f290
You Taein <ytn1574@gmail.com>
2022-09-24 13:15:22 +0900

    part1 1일차

---
+++ src/springbook/user/dao/UserDao.java
@@ -0,0 +1,59 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+
+/*JDBC를 이용하는 작업의 일반적인 순서
+ *DB 연결을 위한 Connection을 가져온다
+ *SQL을 담은 Statement(혹은 PreparedStatement)를 만든다
+ *만들어진 Statement를 실행한다
+ *조회의 경우 SQL쿼리의 실행 결과를 ResultSet으로 받아서 정보를 저장할 오브젝트에 옮겨준다
+ *작업중에 생성된 Connection, Statement, ResultSet같은 리소스는 작업을 마친 후 닫아준다
+ *JDBC API가 만들어내는 예외를 잡아서 직접 처리하거나, 메소드에 throws를 선언해서 예외가 발생하면 메소드 밖으로 던지게 한다 
+ */
+public class UserDao {
+	
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection c = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "spring", "spring");
+		
+		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection c = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "spring", "spring");
+		
+		PreparedStatement ps = c.prepareStatement("select * from users where id=?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+	
+
+}
---
+++ src/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,30 @@
+package springbook.user.dao;
+
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("testid2");
+		user.setName("testname2");
+		user.setPassword("testpassword2");
+		
+		dao.add(user);
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = new User();
+		dao.get(user.getId());
+		System.out.println(user2.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + " 조회 성공");
+		
+	}
+
+}
